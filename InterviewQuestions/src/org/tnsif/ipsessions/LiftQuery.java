de4508d494a5d179f20de6e0013e129f6ce0a52b
novinakoli <kolinovina@gmail.com>
2024-04-25 18:59:16 +0530

    first commit

---
+++ InterviewQuestions/src/org/tnsif/ipsessions/LiftQuery.java
@@ -0,0 +1,32 @@
+package org.tnsif.ipsessions;
+
+import java.util.Scanner;
+
+public class LiftQuery {
+	public static void LiftQueries(int a, int b, int n) {
+		int call;
+		Scanner sc = new Scanner(System.in);
+		while (n != 0) {
+			System.out.println("Enter the test case");
+			call = sc.nextInt();
+
+			if (Math.abs(call - a) <= Math.abs(call - b)) {
+				System.out.println("A");
+				a = call;
+			} else {
+				System.out.println("B");
+				b = call;
+			}
+			n--;
+		}
+	}
+
+	public static void main(String[] args) {
+		int a = 1, b = 7, n;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of test case");
+		n = sc.nextInt();
+		LiftQueries(a, b, n);
+	}
+
+}
